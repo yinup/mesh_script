ceaf256700b7e84c678904553ef44076b7ff4e18
yinhx <yin_up@163.com>
2018-12-27 10:47:47 -0500

    Add IP setting

--- mesh_start.sh
+++ mesh_start.sh
@@ -1,9 +1,22 @@
 #! /bin/bash
 
+#Set IP
+ip_prefix="10.1.1."
+name=`hostname`
+ip_suffix="${name//[!0-9]/}"
+ip_address=${ip_prefix}${ip_suffix}/24
+
+
 pkill wpa_supplicant
 
+#Enable Mesh Node
 iw dev wlan0 interface add mesh0 type mp mesh_id mnet
 iw dev mesh0 set channel 1
 ifconfig wlan0 down
 ifconfig mesh0 up
-ip addr add 10.1.100.10/24 dev mesh0
+ip addr add ${ip_address} dev mesh0
+
+
+
+
+
